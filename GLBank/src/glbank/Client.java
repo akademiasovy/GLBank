e4078307931ed18454d06be8c74db28da654e8ec
Roland <roland.onofrej@akademiasovy.sk>
2017-03-23 13:22:05 +0100

    changed sql script, new class Client

--- GLBank/src/glbank/Client.java
+++ GLBank/src/glbank/Client.java
@@ -10,5 +10,29 @@ package glbank;
  * @author client
  */
 public class Client {
+    private int idc;
+    private String lastname;
+    private String firstname;
+    private String email;
+    private String street;
+    private int housenumber;
+    private String postcode;
+    private String username;
+    private boolean disable;
+    private boolean blocked;
+
+    public Client(int idc, String lastname, String firstname, String email, String street, int housenumber, String postcode, String username, boolean disable, boolean blocked) {
+        this.idc = idc;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.email = email;
+        this.street = street;
+        this.housenumber = housenumber;
+        this.postcode = postcode;
+        this.username = username;
+        this.disable = disable;
+        this.blocked = blocked;
+    }
+    
     
 }
